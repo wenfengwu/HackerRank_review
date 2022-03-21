b689c7a8685532cc9d8079a40ce46c3317d1ee24
wenfengwu <wenfengwu92@gmail.com>
2022-03-20 19:56:50 -0700

    0320

---
+++ Diagonal_Difference.py
@@ -0,0 +1,44 @@
+# Given a square matrix, calculate the absolute difference between the sums of its diagonals.
+
+
+import math
+import os
+import random
+import re
+import sys
+
+#
+# Complete the 'diagonalDifference' function below.
+#
+# The function is expected to return an INTEGER.
+# The function accepts 2D_INTEGER_ARRAY arr as parameter.
+#
+
+def diagonalDifference(arr):
+    # Write your code here
+    n = len(arr)
+    left = 0
+    right = 0
+    for i in range(n):
+        left += arr[i][i]
+    for j in range(n):
+        right += arr[j][n-j-1]
+    
+    return abs(left - right)
+    
+
+if __name__ == '__main__':
+    fptr = open(os.environ['OUTPUT_PATH'], 'w')
+
+    n = int(input().strip())
+
+    arr = []
+
+    for _ in range(n):
+        arr.append(list(map(int, input().rstrip().split())))
+
+    result = diagonalDifference(arr)
+
+    fptr.write(str(result) + '\n')
+
+    fptr.close()
