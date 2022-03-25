02d78c714eb48be29dbbd1f943f19ceefa8d8494
wenfengwu <wenfengwu92@gmail.com>
2022-03-24 20:01:59 -0700

    0

---
+++ PreparationKit/Queue_using_Two_Stacks.py
@@ -0,0 +1,28 @@
+# # Enter your code here. Read input from STDIN. Print output to STDOUT
+
+# q = int(input())
+# stack1 = []
+# stack2 = []
+# for i in range(q):
+#     operation = list(input().split())
+#     #enqueue
+#     if operation[0] == '1':
+#         stack1.append(operation[1])
+    
+#     #dequeue
+#     elif operation[0] == '2':
+#         if not stack2:
+#             while stack1:
+#                 stack2.append(stack1.pop())
+                
+#         stack2.pop()
+        
+#     else:
+#         if not stack2:
+#             while stack1:
+#                 stack2.append(stack1.pop())
+        
+#         print(stack2[-1])
+    
+
+    
