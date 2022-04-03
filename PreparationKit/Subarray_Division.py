e2ad32e4982ea52fba699222f0eec852cabaf48f
wenfengwu <wenfengwu92@gmail.com>
2022-04-02 22:31:11 -0700

    0402

---
+++ PreparationKit/Subarray_Division.py
@@ -0,0 +1,51 @@
+import math
+import os
+import random
+import re
+import sys
+
+#
+# Complete the 'birthday' function below.
+#
+# The function is expected to return an INTEGER.
+# The function accepts following parameters:
+#  1. INTEGER_ARRAY s
+#  2. INTEGER d
+#  3. INTEGER m
+#
+
+def birthday(s, d, m):
+    # Write your code here
+    #use sliding window
+    result = 0
+    total = sum(s[0:m])
+    if total == d:
+        result += 1
+    
+    for i in range(m, len(s)):
+        total += s[i]
+        total -= s[i-m]
+        if total == d:
+            result += 1
+    return result
+
+if __name__ == '__main__':
+    fptr = open(os.environ['OUTPUT_PATH'], 'w')
+
+    n = int(input().strip())
+
+    s = list(map(int, input().rstrip().split()))
+
+    first_multiple_input = input().rstrip().split()
+
+    d = int(first_multiple_input[0])
+
+    m = int(first_multiple_input[1])
+
+    result = birthday(s, d, m)
+
+    fptr.write(str(result) + '\n')
+
+    fptr.close()
+
+    
